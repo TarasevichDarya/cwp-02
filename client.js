639b7f421ae38fca36644d33a991a646863e6151
Darya <tarasevichdarya@yandex.by>
2018-09-26 22:21:17 +0300

    Create new variable in client.js and function which read qa.json and question as an array

--- client.js
+++ client.js
@@ -1,21 +1,23 @@
 // client.js
 const net = require('net');
+const fs = require('fs');
 const port = 8124;
+const string = 'QA';
+const bad = 'DEC';
+const good = 'ACK';
 
 const client = new net.Socket();
-
+let currentIndex = -1;
 client.setEncoding('utf8');
 
-client.connect(port, function() {
-  console.log('Connected');
-  client.write('\r\nHello, Server!\r\nLove,\r\nClient.\r\n');
+let questions = [];
+client.connect({port: port, host: '127.0.0.1'}, () => {
+    fs.readFile("qa.json", (err, text) => {
+        if (!err) {
+            questions = JSON.parse(text);
+            client.write(string);
+        }
+        else console.error(err);
+    });
 });
 
-client.on('data', function(data) {
-  console.log(data);
-  client.destroy();
-});
-
-client.on('close', function() {
-  console.log('Connection closed');
-});
